1aa9d051cf3e6d8f330c6802e6670555733aeb35
diimass26 <diimassadrian26@gmail.com>
2024-10-18 23:09:35 +0700

    update server

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.send('GET request to homepage')
 })
 
+app.get('/dimas', (req, res) => {
+  res.send('DIMAS ANJAY MABAR')
+})
+
 app.listen(port, () => {console.log(`server running to port ${port}`)})
